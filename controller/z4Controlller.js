8e7387bfab38ba2edbe543bf178565d32a289233
dercko4 <dercko4@mail.ru>
2024-01-24 15:54:05 +0300

    ServerZad2

--- controller/z4Controlller.js
+++ controller/z4Controlller.js
@@ -9,12 +9,41 @@ class z4Controller
     async update_todo(req, res)
     {
         const data = req.body
-        const {id} = req.query
-        const update_rows = await sequelize.query(`
+        const {id} = req.params
+        if(!id) return res.json({messenge: "Вы не ввели какой ID нужно редактировать!"})
+        if(!data.id)
+        {
+            const update_rows = await sequelize.query(`
+        UPDATE "ToDos" SET title='${data.title}', description='${data.description}', "isDone"=${data.isDone} WHERE id=${id}
+        `)
+        }
+        if(!data.title)
+        {
+            const update_rows = await sequelize.query(`
+        UPDATE "ToDos" SET id='${data.id}', description='${data.description}', "isDone"=${data.isDone} WHERE id=${id}
+        `)
+        }
+        if(!data.description)
+        {
+            const update_rows = await sequelize.query(`
+        UPDATE "ToDos" SET id='${data.id}', title='${data.title}', "isDone"=${data.isDone} WHERE id=${id}
+        `)
+        }
+        if(!data.isDone)
+        {
+            const update_rows = await sequelize.query(`
+        UPDATE "ToDos" SET id='${data.id}', title='${data.title}', description='${data.description}' WHERE id=${id}
+        `)
+        }
+        if(data.id!==undefined&&data.title!==undefined&&data.description!==undefined&&data.isDone!==undefined)
+        {
+            const update_rows = await sequelize.query(`
         UPDATE "ToDos" SET id='${data.id}', title='${data.title}', description='${data.description}', "isDone"=${data.isDone} WHERE id=${id}
         `)
+        }
         return res.json(update_rows)
     }
+    
 }
 
 module.exports= new z4Controller()
--- router/z4Router.js
+++ router/z4Router.js
@@ -2,6 +2,6 @@ const Router = require('express')
 const routes = new Router()
 const z4Controlller = require('../controller/z4Controlller')
 
-routes.patch('/update', z4Controlller.update_todo)
+routes.patch('/update/:id', z4Controlller.update_todo)
 
 module.exports=routes
