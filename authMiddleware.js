0da369386c8876baae87a392c82458bb97a2573d
dercko4 <dercko4@mail.ru>
2024-02-06 12:19:15 +0300

    New

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,18 +1,29 @@
-const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 
 module.exports = function (req, res, next) {
 if (req.method === "OPTIONS") {
 next()
 }
 try {
-const token = req.headers.authe.split(' ')[1] // Bearer token
-if (!token) {
-return res.status(401).json({message: "Не авторизован"})
-}
-const decoded = jwt.verify(token, process.env.SECRET_KEY)
-req.user = decoded
-next()
-} catch (e) {
+    if (req.headers.authorization) {
+        let tokenParts = req.headers.authorization
+            .split(' ')[1]
+            .split('.');
+        let signature = crypto
+            .createHmac('SHA256', process.env.SECRET_KEY)
+            .update(`${tokenParts[0]}.${tokenParts[1]}`)
+            .digest('base64');
+        let sig1 = signature.slice(0, -1) 
+        if (sig1 === tokenParts[2])
+            req.user = JSON.parse(
+                Buffer.from(
+                    tokenParts[1],
+                    'base64'
+                ).toString('utf8')
+            )
+                
+        next()
+}} catch (e) {
 res.status(401).json({message: "Не авторизован"})
 }
-};
+}
